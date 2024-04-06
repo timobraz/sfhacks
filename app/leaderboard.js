16ecfefc5399c421b0449997e326fdc847d20598
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-04-06 15:08:32 -0700

    Refactor leaderboard component in app/leaderboard.js

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,19 +1,25 @@
 import { View, Text } from "react-native";
+
 import Navbar from "../components/navbar";
 import React from "react";
 
 const Leaderboard = () => {
   return (
-    <View className="Flex-1">
-      <View className="h-full w-full flex flex-col bg-slate-900">
-        <View>
-          <Text className="mt-7 text-white text-[45px] tracking-[10px] text-center font-[Koulen]">
-            Leaderboard
-          </Text>
+    <>
+      <View className="flex-1">
+        <View className="h-full w-full flex flex-col bg-slate-900">
+          <View>
+            <Text className="my-7 text-white text-[45px] tracking-[10px] text-center font-[Koulen]">
+              Leaderboard
+            </Text>
+          </View>
+          <View className="w-full px-7 py-5">
+            <View></View>
+          </View>
         </View>
+        <Navbar />
       </View>
-      <Navbar />
-    </View>
+    </>
   );
 };
 
