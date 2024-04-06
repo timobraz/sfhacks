a3d21292981f536e7511699eb6b839807fa927ca
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-04-06 10:54:37 -0700

    model

--- app/index.js
+++ app/index.js
@@ -14,15 +14,15 @@ const Feed = () => {
         <View className="font-[Koulen] h-screen   ">
           <Text className="font-[Koulen] text-[50px] tracking-[20px]  text-center pt-5">Navigation</Text>
           <View className="-space-y-44 flex h-screen ">
-            <TouchableOpacity className="bg-pink-300 w-full h-44  rounded-t-[48px] p-8 -mb-8" onPress={() => router.push("/feed")}>
+            <TouchableOpacity className="bg-pink-300 w-full h-44  rounded-t-[48px] p-8 -mb-10" onPress={() => router.push("/feed")}>
               <Text className="text-5xl font-extrabold ">Upload</Text>
               <Text className="text-2xl tracking-[-1px]">Take a pic of your trash</Text>
             </TouchableOpacity>
-            <View className="bg-slate-800 w-full h-44  rounded-t-[48px] p-8 -my-4">
+            <View className="bg-slate-800 w-full h-44  rounded-t-[48px] p-8 -mb-10">
               <Text className="text-5xl font-extrabold text-orange-100">Leaderboard</Text>
               <Text className="text-2xl tracking-[-1px] text-orange-100">Take a pic of your trash</Text>
             </View>
-            <View className="bg-yellow-300 w-full h-44  rounded-t-[48px] p-8 -my-4">
+            <View className="bg-yellow-300 w-full h-44  rounded-t-[48px] p-8 -mb-10">
               <Text className="text-5xl font-extrabold ">Heat Map</Text>
               <Text className="text-2xl tracking-[-1px]">Take a pic of your trash</Text>
             </View>
