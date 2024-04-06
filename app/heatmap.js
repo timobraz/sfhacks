6719e41ec2ced05af6eb35a1b8867dbc9ae3d249
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-04-05 22:45:36 -0700

    structure

---
+++ app/heatmap.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heatmap() {
+  return <div>Heatmap</div>;
+}
+
+export default Heatmap;
---
+++ app/index.js
@@ -0,0 +1,40 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+
+const Feed = () => {
+  const [fontsLoaded] = useFonts({
+    Koulen: require("../assets/fonts/Koulen.ttf"),
+  });
+  {
+    return !fontsLoaded ? null : (
+      <View className=" bg-yellow-100 h-full flex flex-1">
+        <View className="font-[Koulen] h-screen   ">
+          <Text className="font-[Koulen] text-[50px] tracking-[20px]  text-center pt-5">Navigation</Text>
+          <View className="-space-y-44 flex h-screen ">
+            <View className="bg-pink-300 w-full h-44  rounded-t-[48px] p-8 -mb-8">
+              <Text className="text-5xl font-extrabold ">Upload</Text>
+              <Text className="text-2xl tracking-[-1px]">Take a pic of your trash</Text>
+            </View>
+            <View className="bg-slate-800 w-full h-44  rounded-t-[48px] p-8 -my-4">
+              <Text className="text-5xl font-extrabold text-orange-100">Leaderboard</Text>
+              <Text className="text-2xl tracking-[-1px] text-orange-100">Take a pic of your trash</Text>
+            </View>
+            <View className="bg-yellow-300 w-full h-44  rounded-t-[48px] p-8 -my-4">
+              <Text className="text-5xl font-extrabold ">Heat Map</Text>
+              <Text className="text-2xl tracking-[-1px]">Take a pic of your trash</Text>
+            </View>
+            <View className="bg-orange-700 w-full  rounded-t-[48px] p-8 -my-4 h-full">
+              <Text className="text-5xl font-extrabold text-orange-100 ">About</Text>
+              <Text className="text-2xl tracking-[-1px] text-orange-100">Take a pic of your trash</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+};
+
+export default Feed;
