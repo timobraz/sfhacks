11dd698ed7bdb33f0c3a97bb4427143ecdfdfb14
Michael Liu <michael.liu.mike@gmail.com>
2024-04-06 17:22:07 -0700

    Update import statements and fix typos in index.js and profile.js

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,20 +1,20 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 
-import About from "./about";
-import Upload from "./upload";
-import Leaderboard from "./leaderboard";
-import Heatmap from "./heatmap";
-import Menu from "./Menu"; // Assuming Menu is in the same directory
-import Feed from "./feed";
-import Profile from "./profile";
-import { useFonts } from "expo-font";
+import About from './about';
+import Upload from './upload';
+import Leaderboard from './leaderboard';
+import Heatmap from './heatmap';
+import Menu from './Menu'; // Assuming Menu is in the same directory
+import Feed from './feed';
+import Profile from './profile';
+import { useFonts } from 'expo-font';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   const [fontsLoaded] = useFonts({
-    Koulen: require("../assets/fonts/Koulen.ttf"),
+    Koulen: require('../assets/fonts/Koulen.ttf'),
   });
 
   if (!fontsLoaded) {
@@ -25,29 +25,25 @@ const App = () => {
       initialRouteName="Menu"
       independent={true}
       screenOptions={{
-        headerBackTitle: "Back",
+        headerBackTitle: 'Back',
         headerBackTitleStyle: {
-          color: "black",
+          color: 'black',
           fontSize: 20,
-          fontWeight: "bold",
+          fontWeight: 'bold',
         },
         headerTitleStyle: {
           fontSize: 20,
-          fontWeight: "800",
+          fontWeight: '800',
         },
       }}
     >
-      <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }}/>
+      <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }} />
       <Stack.Screen name="About" component={About} />
       <Stack.Screen name="Upload" component={Upload} />
       <Stack.Screen name="Leaderboard" component={Leaderboard} />
       <Stack.Screen name="Heatmap" component={Heatmap} />
       <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen
-        name="Feed"
-        component={Feed}
-        options={{ headerShown: false }}
-      />
+      <Stack.Screen name="Feed" component={Feed} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
