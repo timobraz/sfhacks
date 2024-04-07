0b9dac657e643ff08d2f50db852b3177d7a07d96
Andrew Hwang <pandy430@gmail.com>
2024-04-06 20:19:33 -0700

    ead

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -24,7 +24,7 @@ const App = () => {
   }
   return (
     <Stack.Navigator
-      initialRouteName="Profile"
+      initialRouteName="Feed"
       independent={true}
       screenOptions={{
         headerBackTitle: 'Back',
