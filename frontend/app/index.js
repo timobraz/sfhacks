e1d455323c52c985727b117b163061427b7568a0
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-04-06 20:16:31 -0700

    minor

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -22,7 +22,7 @@ const App = () => {
   }
   return (
     <Stack.Navigator
-      initialRouteName="Profile"
+      initialRouteName="Upload"
       independent={true}
       screenOptions={{
         headerBackTitle: 'Back',
