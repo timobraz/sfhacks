31ab02836f37d719e79ce5b302363942c91eec7b
Michael Liu <michael.liu.mike@gmail.com>
2024-04-07 05:22:47 -0700

    Loading State for modal image

--- frontend/app/uploadNext.js
+++ frontend/app/uploadNext.js
@@ -1,102 +1,113 @@
-
 import React, { useState, useEffect, useRef } from 'react';
 import Navbar from '../components/navbar';
-import { View, Text, TouchableOpacity, StatusBar, Dimensions, Pressable, SafeAreaView, TextInput, Modal, Keyboard,  TouchableWithoutFeedback} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+  Dimensions,
+  Pressable,
+  SafeAreaView,
+  TextInput,
+  Modal,
+  Keyboard,
+  TouchableWithoutFeedback,
+  ActivityIndicator,
+} from 'react-native';
 import { Image } from 'expo-image';
 import { router, useLocalSearchParams } from 'expo-router';
 import axios from 'axios';
 
 function UploadNext() {
-  const {imageData} = useLocalSearchParams();
-  const [description, setDescription] = useState("");
+  const { imageData } = useLocalSearchParams();
+  const [description, setDescription] = useState('');
   const blurhash =
-  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
-const [modalVisible, setModalVisible] = useState(false);
- const [data,setData]=useState({});
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+  const [modalVisible, setModalVisible] = useState(false);
+  const [data, setData] = useState({});
   const image = require('../assets/vectorThing.png');
 
   const pressed = async () => {
     try {
-      const res = await axios.post("https://concise-hookworm-utterly.ngrok-free.app/upload", {
+      const res = await axios.post('https://concise-hookworm-utterly.ngrok-free.app/upload', {
         image: imageData,
         description: description,
       });
-      if (res.data){
+      if (res.data) {
         console.log(res.data);
         setData(res.data);
         setModalVisible(true);
       }
-     
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   const DismissKeyboard = ({ children }) => (
-    <TouchableWithoutFeedback 
-    onPress={() => Keyboard.dismiss()}>{children}
-    </TouchableWithoutFeedback>
-    );
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>{children}</TouchableWithoutFeedback>
+  );
   return (
     <>
-    <SafeAreaView onPress={() => Keyboard.dismiss()} className="h-[2000px] w-full flex items-center flex-col bg-[#D7BCED] -mt-16">
-      
-
-      <View className="flex-row gap-5 items-center px-11 pb-10 h-auto mt-2">
-        <View className="h-full w-full bg-[#E9E9E9] rounded-xl flex flex-col items-center px-5">
-          <Text className="text-2xl font-[Koulen] pt-6 color-[#2F2F2F]">
-            Describe what you did...
-          </Text>
-          <View className="w-full bg-[#D9D9D9]">
-            <TextInput className="w-full h-[80px] font-[RobotoMedium] p-2" placeholder="I went to the park and"
-                      multiline = {true}
-                      numberOfLines = {4} 
-                      onChange={(e) => {setDescription(e.nativeEvent.text)}}
-                      value={description}>
-
-            </TextInput>
+      <SafeAreaView
+        onPress={() => Keyboard.dismiss()}
+        className="h-[2000px] w-full flex items-center flex-col bg-[#D7BCED] -mt-16"
+      >
+        <View className="flex-row gap-5 items-center px-11 pb-10 h-auto mt-2">
+          <View className="h-full w-full bg-[#E9E9E9] rounded-xl flex flex-col items-center px-5">
+            <Text className="text-2xl font-[Koulen] pt-6 color-[#2F2F2F]">Describe what you did...</Text>
+            <View className="w-full bg-[#D9D9D9]">
+              <TextInput
+                className="w-full h-[80px] font-[RobotoMedium] p-2"
+                placeholder="I went to the park and"
+                multiline={true}
+                numberOfLines={4}
+                onChange={(e) => {
+                  setDescription(e.nativeEvent.text);
+                }}
+                value={description}
+              ></TextInput>
+            </View>
           </View>
         </View>
-        
 
-      </View>
-      
-      <View className="flex-row gap-5 items-center px-8 pl-[54px] h-[450px]">
-              <DismissKeyboard>
-              <Image
-                source={{ uri: `data:image/jpeg;base64,${imageData}`}}
-                placeholder={blurhash}
-                contentFit="cover"
-                transition={1000}
-                className="w-full h-full rounded-lg"
-              >
-              </Image>      
+        <View className="flex-row gap-5 items-center px-8 pl-[54px] h-[450px]">
+          <DismissKeyboard>
+            <Image
+              source={{ uri: `data:image/jpeg;base64,${imageData}` }}
+              placeholder={blurhash}
+              contentFit="cover"
+              transition={1000}
+              className="w-full h-full rounded-lg"
+            ></Image>
           </DismissKeyboard>
-
-      </View>
-      <TouchableOpacity className='flex flex-row justify-center gap-2 items-center' onPress={()=>{pressed(); setModalVisible(true)}}>
-        <Text className="color-[#FFFFFF] font-Koulen text-8xl pt-10">
-          SEND
-        </Text>
-        <View className='w-[60px] h-[60px] mb-11'>
-          <Image
-            source={image}
-            placeholder='asdf'
-            contentFit="cover"
-            transition={10}
-            className="w-full h-full"
-          >
-          </Image>
         </View>
-      </TouchableOpacity>
+        <TouchableOpacity
+          className="flex flex-row justify-center gap-2 items-center"
+          onPress={() => {
+            pressed();
+            setModalVisible(true);
+          }}
+        >
+          <Text className="color-[#FFFFFF] font-Koulen text-8xl pt-10">SEND</Text>
+          <View className="w-[60px] h-[60px] mb-11">
+            <Image
+              source={image}
+              placeholder="asdf"
+              contentFit="cover"
+              transition={10}
+              className="w-full h-full"
+            ></Image>
+          </View>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <FinalResultModal isVisible={modalVisible} setModalVisible={setModalVisible} data={data} />
 
-    </SafeAreaView>
-          <FinalResultModal isVisible={modalVisible} setModalVisible={setModalVisible} data={data} />
-
-    <Navbar />
+      <Navbar />
     </>
   );
 }
-const FinalResultModal = ({ isVisible, setModalVisible,data }) => {
+const FinalResultModal = ({ isVisible, setModalVisible, data }) => {
+  const [imageLoading, setImageLoading] = useState(true);
+
   const handlePress = () => {
     setModalVisible(false);
     router.push('/feed');
@@ -106,12 +117,24 @@ const FinalResultModal = ({ isVisible, setModalVisible,data }) => {
       <Modal animationType="slide" transparent={true} visible={isVisible}>
         <View className="flex-1 justify-center items-center bg-black bg-opacity-20">
           <View className="bg-white p-4 rounded-lg">
+            {imageLoading && (
+              <View className="w-full justify-center items-center transform translate-x-44 translate-y-64">
+                <ActivityIndicator size="large" color="#0000ff" />
+                {/* <Text className="text-center font-bold text-5xl transform translate-x-24 translate-y-64">
+                  Loading...
+                </Text> */}
+              </View>
+            )}
             <Image
               source={{ uri: data.img }}
               width={380}
               height={500}
               borderRadius={10}
               marginBottom={16}
+              onLoad={() => {
+                // console.log('Image success');
+                setImageLoading(false);
+              }}
             />
             <Text className="text-xl font-bold text-center mb-2">
               You've earned <Text className="text-emerald-500">{data.points}</Text> points!
@@ -132,5 +155,6 @@ const FinalResultModal = ({ isVisible, setModalVisible,data }) => {
         </View>
       </Modal>
     </>
-  );};
+  );
+};
 export default UploadNext;
