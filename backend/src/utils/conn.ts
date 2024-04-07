933999da70063599b7efdf2dbd42358313af9f2a
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-04-06 19:50:55 -0700

    working upload

--- backend/src/utils/conn.ts
+++ backend/src/utils/conn.ts
@@ -1,14 +1,8 @@
-import { Db, MongoClient } from 'mongodb';
-async function connect(): Promise<Db | undefined> {
-  const connectionString = process.env.MONGODB_URI!;
-  console.log(connectionString);
-  try {
-    const client = new MongoClient(connectionString);
-    const conn = await client.connect();
-    let db = conn?.db('cleanASF');
-    return db;
-  } catch (e) {
-    console.error(e);
-  }
+import mongoose from 'mongoose';
+
+export function connect() {
+  mongoose.connect(process.env.MONGODB_URI as string, {
+    dbName: 'cleanASF',
+  });
+  return mongoose.connection;
 }
-export default connect;
